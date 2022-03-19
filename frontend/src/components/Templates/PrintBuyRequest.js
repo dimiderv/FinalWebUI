68c1f7f4460102a61b6d4127ba87107b2267e1b8
dimiderv <dimiderv@gmail.com>
2022-03-20 00:53:16 +0200

    Add files via upload

--- frontend/src/components/Templates/PrintBuyRequest.js
+++ frontend/src/components/Templates/PrintBuyRequest.js
@@ -10,6 +10,9 @@ function PrintBuyRequest(props) {
                     <h6 className="card-title">Asset to buy: {props.assetID}</h6>
                     <p className="card-text">Buyer is: {props.buyerID}</p>
                     <p className="card-text">Buyer MSP is: {props.buyerMSP}</p>
+                    <p className="card-text">Timestamp: {props.timestamp}</p>
+                    <p className="card-text">TxId: {props.txId}</p>
+                    <p className="card-text">Salt: {props.salt}</p>
                 </div>
             </div>
         </div>
